7b59d042695275fc7e59ee3984bbb5693507ea1d
morningroad <78800174+morningroad@users.noreply.github.com>
2026-07-23 22:48:57 +0800

    Update smb5-lib-munch.c

--- drivers/power/supply/qcom/smb5-lib-munch.c
+++ drivers/power/supply/qcom/smb5-lib-munch.c
@@ -3112,7 +3112,7 @@ int smblib_get_prop_batt_charge_done(struct smb_charger *chg,
 			if ((smblib_get_fastcharge_mode(chg) == true)
 				&& (pval.intval >= 98))
 				smblib_set_fastcharge_mode(chg, false);
-				return 0;
+			return 0;
 		}
 
 		if (smblib_get_fastcharge_mode(chg) == true)
@@ -10485,9 +10485,10 @@ static void smblib_charger_type_recheck(struct work_struct *work)
 		return;
 	}
 
-	if (smblib_get_prop_dfp_mode(chg) != POWER_SUPPLY_TYPEC_NONE)
+	if (smblib_get_prop_dfp_mode(chg) != POWER_SUPPLY_TYPEC_NONE) {
 		goto check_next;
-
+    }
+	
 		if (chg->typec_port && !chg->pr_swap_in_progress) {
 
 			/*
